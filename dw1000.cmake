cd4ffdb74b77436751d80f78551bc947fdc50502
Xuan Tho Do <tho.dx@novaintechs.com>
2024-02-28 21:51:53 +0700

    feat: add dw1000 lib cmake

---
+++ dw1000.cmake
@@ -0,0 +1,41 @@
+function(add_dw1000_lib)
+    set(oneValueArgs PATH TARGET_NAME)
+    set(multiValueArgs EXTRA_INCLUDES DEPENDENCIES)
+    set(options)
+
+    cmake_parse_arguments(DW1000 "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
+
+    add_library(${DW1000_TARGET_NAME} STATIC)
+
+    target_sources(
+        ${DW1000_TARGET_NAME}
+        PRIVATE
+        ${DW1000_PATH}/src/DW1000.cpp
+        ${DW1000_PATH}/src/DW1000Device.cpp
+        ${DW1000_PATH}/src/DW1000Mac.cpp
+        ${DW1000_PATH}/src/DW1000Ranging.cpp
+        ${DW1000_PATH}/src/DW1000Time.cpp
+    )
+
+    # Extra includes
+    foreach(INC ${DW1000_EXTRA_INCLUDES})
+        target_include_directories(${DW1000_TARGET_NAME}
+            PUBLIC
+            ${INC}
+        )
+    endforeach()
+
+    target_include_directories(
+        ${DW1000_TARGET_NAME}
+        SYSTEM
+        PUBLIC
+        ${DW1000_PATH}/src
+    )
+
+    # Link dependencies libraries
+    foreach(DEPENDENCY ${DW1000_DEPENDENCIES})
+        target_link_libraries(${DW1000_TARGET_NAME}
+            ${DEPENDENCY}
+        )
+    endforeach()
+endfunction()
