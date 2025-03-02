32cd4816edb1ef7c0d3d8bccd912cc293d1c9e03
Xuan Tho Do <tho.dx@novaintechs.com>
2025-03-02 17:31:22 +0700

    feat: support preprocessor

--- jsmn.cmake
+++ jsmn.cmake
@@ -1,11 +1,19 @@
 function(add_jsmn_lib)
     set(oneValueArgs PATH TARGET_NAME)
-    set(multiValueArgs EXTRA_INCLUDES DEPENDENCIES)
+    set(multiValueArgs EXTRA_INCLUDES PREPROCESSOR DEPENDENCIES)
     set(options)
 
     cmake_parse_arguments(JSMN "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
 
     add_library(${JSMN_TARGET_NAME} INTERFACE)
+
+    # Preprocessor
+    foreach(PP ${JSMN_PREPROCESSOR})
+        target_compile_definitions(${JSMN_TARGET_NAME}
+            INTERFACE
+            ${PP}
+        )
+    endforeach()
 
     target_include_directories(
         ${JSMN_TARGET_NAME}
